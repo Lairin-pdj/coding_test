62faf872e98d89fdca7cfe8a69a164d98d98bb73
Dong-jun Park <70496139+Lairin-pdj@users.noreply.github.com>
2021-11-16 22:20:06 +0900

    Create 18258_큐 2.py

---
+++ baekjoon/18258_큐 2.py
@@ -0,0 +1,36 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+# 파싱
+n = int(input())
+
+# 쿼리 진행
+queue = deque()
+for _ in range(n):
+    query = input().rstrip().split()
+    
+    if query[0] == "push":
+        queue.append(int(query[1]))
+    elif query[0] == "pop":
+        if queue:
+            print(queue.popleft())
+        else:
+            print(-1)
+    elif query[0] == "size":
+        print(len(queue))
+    elif query[0] == "empty":
+        if queue:
+            print(0)
+        else:
+            print(1)
+    elif query[0] == "front":
+        if queue:
+            print(queue[0])
+        else:
+            print(-1)
+    else:
+        if queue:
+            print(queue[-1])
+        else:
+            print(-1)
