7e15a0ef1c5481909efdb27f2cf7714dbd230d26
Dong-jun Park <70496139+Lairin-pdj@users.noreply.github.com>
2021-11-24 21:00:24 +0900

    Create 1057_토너먼트.py

---
+++ baekjoon/1057_토너먼트.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+# 파싱
+n, a, b = map(int, input().split())
+
+count = 0
+while a != b:
+    count += 1
+    a = (a + 1) // 2
+    b = (b + 1) // 2
+    
+print(count)
