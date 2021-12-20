476910b5fce8b06d0c45bb0ffebcbb30996dc1cc
Dong-jun Park <70496139+Lairin-pdj@users.noreply.github.com>
2021-12-20 16:08:06 +0900

    Create 10845_큐.py

---
+++ baekjoon/10845_큐.py
@@ -0,0 +1,34 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+# 파싱
+n = int(input())
+queue = deque()
+for _ in range(n):
+    query = input().rstrip().split()
+    
+    if query[0] == "push":
+        queue.append(query[1])
+    elif query[0] == "pop":
+        if queue:
+            print(queue.popleft())
+        else:
+            print(-1)
+    elif query[0] == "size":
+        print(len(queue))
+    elif query[0] == "empty":
+        if queue:
+            print("0")
+        else:
+            print("1")
+    elif query[0] == "front":
+        if queue:
+            print(queue[0])
+        else:
+            print("-1")
+    elif query[0] == "back":
+        if queue:
+            print(queue[-1])
+        else:
+            print("-1")
