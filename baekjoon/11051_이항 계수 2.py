c98e09c81c07ab92696b4b09648907961ceb8d60
Dong-jun Park <70496139+Lairin-pdj@users.noreply.github.com>
2022-01-22 18:23:13 +0900

    Create 11051_이항 계수 2.py

---
+++ baekjoon/11051_이항 계수 2.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+# 파싱
+n, k = map(int, input().split())
+
+temp = 1
+for i in range(n, n - k, -1):
+    temp *= i
+for i in range(1, k + 1):
+    temp = temp // i
+print(temp % 10007)
