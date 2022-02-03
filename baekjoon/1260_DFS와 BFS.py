4e938d475b0a61df24dc65fd6abbfc4d5e2d3785
Dong-jun Park <70496139+Lairin-pdj@users.noreply.github.com>
2022-02-03 21:33:40 +0900

    Create 1260_DFS와 BFS.py

---
+++ baekjoon/1260_DFS와 BFS.py
@@ -0,0 +1,44 @@
+from collections import defaultdict, deque
+import sys
+input = sys.stdin.readline
+
+# 파싱
+n, m, v = map(int, input().split())
+dic = defaultdict(list)
+for _ in range(m):
+    a, b = map(int, input().split())
+    dic[a].append(b)
+    dic[b].append(a)
+
+# 정렬
+for i in dic.keys():
+    dic[i].sort()
+
+# dfs
+def dfs(node):
+    print(node, end = " ")
+    
+    for i in dic[node]:
+        if i not in check:
+            check.add(i)
+            dfs(i)
+
+# dfs 결과 출력
+check = set()
+check.add(v)
+dfs(v)
+print()
+
+# bfs 및 결과 출력
+check = set()
+check.add(v)
+queue = deque()
+queue.append(v)
+while queue:
+    temp = queue.popleft()
+    print(temp, end = " ")
+    
+    for i in dic[temp]:
+        if i not in check:
+            check.add(i)
+            queue.append(i)
