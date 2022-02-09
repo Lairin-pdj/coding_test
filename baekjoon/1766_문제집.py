b0f253c46e984acd9610a299d1595f4b08104f40
Dong-jun Park <70496139+Lairin-pdj@users.noreply.github.com>
2022-02-09 14:42:24 +0900

    Create 1766_문제집.py

---
+++ baekjoon/1766_문제집.py
@@ -0,0 +1,35 @@
+from collections import defaultdict
+import sys, heapq
+input = sys.stdin.readline
+
+# 파싱
+n, m = map(int, input().split())
+dic = defaultdict(list)
+check = [0 for _ in range(n + 1)]
+for _ in range(m):
+    a, b = map(int, input().split())
+    dic[a].append(b)
+    check[b] += 1
+
+# 위상정렬
+# 시작 가능 지점 체크 및 큐에 삽입
+queue = []
+for i in range(1, n + 1):
+    if check[i] == 0:
+        heapq.heappush(queue, i)
+
+# 정점 진행
+answer = []
+while queue:
+    now = heapq.heappop(queue)
+    answer.append(now)
+    
+    for i in dic[now]:
+        if check[i] == 1:
+            check[i] = 0
+            heapq.heappush(queue, i)
+        else:
+            check[i] -= 1
+
+# 결과 출력
+print(*answer)
