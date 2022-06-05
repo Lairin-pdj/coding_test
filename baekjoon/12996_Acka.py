c04df1d73e66637facd0769e9956375395dafc4d
Dong-jun Park <70496139+Lairin-pdj@users.noreply.github.com>
2022-06-05 11:18:38 +0900

    Update 12996_Acka.py

--- baekjoon/12996_Acka.py
+++ baekjoon/12996_Acka.py
@@ -34,3 +34,52 @@ else:
 
     # 결과 출력
     print(answer)
+    
+'''
+# 재귀를 이용한 dp 풀이
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(6250000)
+
+# 파싱
+s, a, b, c = map(int, input().split())
+
+# dp
+def rec(s, a, b, c):
+    
+    # 시작값
+    if s == 0:
+        if a == 0 and b == 0 and c == 0:
+            return 1
+        else:
+            return 0
+    
+    # 접근 불가 값 제거
+    if a < 0 or b < 0 or c < 0:
+        return 0
+    
+    # 중복 체크 제거
+    if dp[s][a][b][c] != -1:
+        return dp[s][a][b][c]
+    
+    # 값 계산
+    temp = rec(s - 1, a - 1, b - 1, c - 1)
+    temp += rec(s - 1, a, b - 1, c - 1)
+    temp += rec(s - 1, a - 1, b, c - 1)
+    temp += rec(s - 1, a - 1, b - 1, c)
+    temp += rec(s - 1, a, b, c - 1)
+    temp += rec(s - 1, a, b - 1, c)
+    temp += rec(s - 1, a - 1, b, c)
+    
+    temp = temp % 1000000007
+    
+    # 결과 반영 및 반환
+    dp[s][a][b][c] = temp
+    return temp
+
+    
+dp = [[[[-1] * (c + 1) for _ in range(b + 1)] for _ in range(a + 1)] for _ in range(s + 1)]
+
+# 결과 출력
+print(rec(s, a, b, c))
+'''
