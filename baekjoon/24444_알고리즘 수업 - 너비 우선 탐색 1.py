b155c296c9973d1f82d87cdd33339cecc1251f0a
Dong-jun Park <70496139+Lairin-pdj@users.noreply.github.com>
2022-07-12 18:03:15 +0900

    Create 24444_알고리즘 수업 - 너비 우선 탐색 1.py

---
+++ baekjoon/24444_알고리즘 수업 - 너비 우선 탐색 1.py
@@ -0,0 +1,32 @@
+from collections import defaultdict, deque
+import sys
+input = sys.stdin.readline
+
+# 파싱
+n, m, r = map(int, input().split())
+dic = defaultdict(list)
+for _ in range(m):
+    u, v = map(int, input().split())
+    
+    # 간선 설정
+    dic[u].append(v)
+    dic[v].append(u)
+
+# bfs
+queue = deque([r])
+check = set([r])
+count = 1
+record = [0] * n
+while queue:
+    now = queue.popleft()
+    record[now - 1] = count
+    count += 1
+    
+    for i in sorted(dic[now]):
+        if i not in check:
+            check.add(i)
+            queue.append(i)
+
+# 출력
+for i in record:
+    print(i)
